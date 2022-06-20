b457a551dba6f115b4b21551a062a66666dcd4ec
sergionjr <seno7509@colorado.edu>
2022-06-20 15:21:49 -0600

    masking html extension through index files, folder

--- js/script.js
+++ js/script.js
@@ -5,25 +5,43 @@ $(document).ready(
 				$('ul li').removeClass('active-link')
 				$(this).addClass('active-link')
 			});
-},
-activeHtmlPage()
+});
 
-);
+$(document).ready(
+	function(){
+		$('.navbar-container .nav-link').bind("click", function(event) {
+			event.preventDefault();
+			var clickedItem = $(this);
+			$('.navbar-container .nav-link').each(function() {
+				$(this).removeClass("active");
+			});
+		});
+	});
+	
+// pages = ['index', 'projects']
+// $('.nav-item').each(function(i) {
+// 	alert("entry into funct")
+// 	if (activeHtmlPage().includes(pages[i])) this.addClass('active-link');
+// 	else if (this.className.includes('active-link')) this.removeClass('active-link');
+//    });
 
+// /*
+// Gets the
+// */
+// function activeHtmlPage(){
+// 	var filePath = window.location.pathname;
+// 	var pageName = filePath.split("/").pop(); 
+// 	//alert(pageName)
+// 	return pageName
+// }
 
+// function yield(){
+// 	pages = ['index', 'projects']
+// 	console.log(activeHtmlPage());
+// $('.nav-link').each(function(i) {
+// 	alert("entry into funct")
+// 	if (activeHtmlPage().includes(pages[i])) this.addClass('active-link');
+// 	else if (this.className.includes('active-link')) this.removeClass('active-link');
+//    });
 
-/*
-Gets the
-*/
-function activeHtmlPage(){
-	var filePath = window.location.pathname;
-	var pageName = filePath.split("/").pop(); 
-	alert(pageName)
-	return pageName
-}
-
-function activeNavLink(){
-	if (activeHtmlPage() == 'index.html'){
-		
-	}
-}
+// }
