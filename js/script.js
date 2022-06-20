d6da4f04395eb8bf5427c388ffc8225f1d0f8169
sergionjr <seno7509@colorado.edu>
2022-06-19 23:03:21 -0600

    fully externalized header, script js incomplete

--- js/script.js
+++ js/script.js
@@ -5,4 +5,25 @@ $(document).ready(
 				$('ul li').removeClass('active-link')
 				$(this).addClass('active-link')
 			});
-});
+},
+activeHtmlPage()
+
+);
+
+
+
+/*
+Gets the
+*/
+function activeHtmlPage(){
+	var filePath = window.location.pathname;
+	var pageName = filePath.split("/").pop(); 
+	alert(pageName)
+	return pageName
+}
+
+function activeNavLink(){
+	if (activeHtmlPage() == 'index.html'){
+		
+	}
+}
