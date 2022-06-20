26c8d8ae4bc07a3594f5cbdff71ac2688c508914
sergionjr <seno7509@colorado.edu>
2022-06-19 18:37:41 -0600

    Attempt to override XMLHttpRequest by pushing file access to github webmaster

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ $(document).ready(
 	function(){
 		$('ul li').click(
 			function(){
-				//alert('click trigger')
 				$('ul li').removeClass('active-link')
 				$(this).addClass('active-link')
 			});
