f97318749084417420040200e366ce84cd70b51a
sergionjr <seno7509@colorado.edu>
2022-06-20 20:31:24 -0600

    defaulting landing page to have index activelink

--- js/header.js
+++ js/header.js
@@ -3,9 +3,5 @@ $(function(){
       $('.nav-link a[href="' +
        window.location.pathname.split('/').pop() +
        '"]', this).parent().addClass('active-link').siblings().removeClass('active-link');
-       alert("asd")
-       console.log(this)
-       console.log(window.location.pathname.split('/').pop())
-       console.log($('.nav-link a'))
     }); 
   });
